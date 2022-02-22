7abd180c17f639ba039531c70010eae6c8db4a20
KiBee <killerbee1205@gmail.com>
2022-02-23 06:29:24 +1200

    complete day_2

--- tasks/builtin_functions/task_1/implementation.py
+++ tasks/builtin_functions/task_1/implementation.py
@@ -1,16 +1,26 @@
 from tasks.common import MyException
+
+
 class Value:
     def __init__(self, value):
         self.value = value
 
-
     def __add__(self, x):
+        return self.value + x
+
+    def __radd__(self, x):
         return self.value + x
 
     def __sub__(self, x):
         return self.value - x
 
+    def __rsub__(self, x):
+        return self.value - x
+
     def __mul__(self, x):
+        return self.value * x
+
+    def __rmul__(self, x):
         return self.value * x
 
     def __truediv__(self, x):
@@ -18,6 +28,12 @@ class Value:
             raise MyException
         return self.value / x
 
+    def __rtruediv__(self, x):
+        if x == 0:
+            raise MyException
+        return self.value / x
+
     def __getattr__(self, x):
         raise MyException
-    pass
+
+
