7bd71507f5eedb91eb2c231cb7b7ddc6c02befa3
KiBee <killerbee1205@gmail.com>
2022-02-26 01:59:52 +1200

    complete day_4(Правки)

--- tasks/magic_method/task_2/implementation.py
+++ tasks/magic_method/task_2/implementation.py
@@ -5,17 +5,24 @@ class MathClock:
     def __add__(self, x):
         self.minutes = self.minutes + x
 
+    def __radd__(self, x):
+        self.minutes = self.minutes + x
+
     def __sub__(self, x):
         self.minutes = self.minutes - x
 
     def __mul__(self, x):
         self.minutes = self.minutes + 60 * x
 
-    def __div__(self, x):
+    def __truediv__(self, x):
+        self.minutes = self.minutes - 60 * x
+
+    def __floordiv__(self, x):
         self.minutes = self.minutes - 60 * x
 
     def get_time(self):
+        self.minutes = int(self.minutes)
         minutes = self.minutes % 60
         hours = self.minutes // 60 % 24
 
-        return "%02d:%02d" % (hours, minutes)
+        return f'{hours:02}:{minutes:02}'
