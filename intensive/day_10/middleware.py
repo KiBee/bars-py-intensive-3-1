fdede548843c6432eb71a37c315064e1dc065b52
KiBee <killerbee1205@gmail.com>
2022-03-06 00:11:39 +1200

    complete day_10

--- intensive/day_10/middleware.py
+++ intensive/day_10/middleware.py
@@ -2,20 +2,36 @@ import json
 
 from django.http import HttpResponse
 from django.http import JsonResponse
+import time
 
 from django.utils.deprecation import (
     MiddlewareMixin,
 )
 
-import sys
+from sys import (
+    getsizeof,
+)
 
 
-class StatisticMiddleware:
+class StatisticMiddleware():
     """
     Компонент вычисляющий время выполнения запроса на сервере и размер ответа в байтах.
     Отображает значения в консоли приложения
     """
-    pass
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        start_time = time.time()
+        response = self.get_response(request)
+
+        request_time = time.time() - start_time
+
+        print(f'Время запроса: {request_time:.5f} сек.')
+        print(f'Размер ответа: {getsizeof(response)} байт')
+
+        return response
 
 
 class FormatterMiddleware:
@@ -23,7 +39,21 @@ class FormatterMiddleware:
     Компонент форматирующий Json ответ в HttpResponse
     {'key': value} => <p>key = value</p>
     """
-    pass
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+
+        if type(response) == JsonResponse:
+            responce_list = []
+            for key, value in json.loads(response.content).items():
+                responce_list.append(f'<p>{key} = {str(value)}</p>')
+
+            response = HttpResponse(''.join(responce_list))
+
+        return response
 
 
 class CheckErrorMiddleware(MiddlewareMixin):
@@ -31,7 +61,7 @@ class CheckErrorMiddleware(MiddlewareMixin):
         Перехватывает необработанное исключение в представлении и отображает ошибку в виде
         "Ошибка: {exception}"
     """
-    pass
 
-
-
+    @staticmethod
+    def process_exception(exception):
+        return HttpResponse(f"Ошибка: {exception}")
--- intensive/day_10/views.py
+++ intensive/day_10/views.py
@@ -1,4 +1,31 @@
 from django.http import JsonResponse
+import ast
+import operator as op
+
+operators = {ast.Add: op.add, ast.Sub: op.sub, ast.Mult: op.mul,
+             ast.Div: op.truediv, ast.Pow: op.pow, ast.BitXor: op.xor,
+             ast.USub: op.neg}
+
+
+def eval_expr(expr):
+    """
+    eval expr и eval - функции для безопасного подсчета
+    выражения в строке (без использования eval())
+    """
+
+    return eval_(ast.parse(expr, mode='eval').body)
+
+
+def eval_(node):
+    if isinstance(node, ast.Num):  # <number>
+        return node.n
+    elif isinstance(node, ast.BinOp):  # <left> <operator> <right>
+        return operators[type(node.op)](eval_(node.left), eval_(node.right))
+    elif isinstance(node, ast.UnaryOp):  # <operator> <operand> e.g., -1
+        return operators[type(node.op)](eval_(node.operand))
+    else:
+        raise TypeError(node)
+
 
 def calc(request):
     """
@@ -10,4 +37,12 @@ def calc(request):
 
     Результат:  JsonResponse вида {'3*3': 9, '10-2': 8, '10/5': 2}
     """
-    pass
+
+    dict_request = dict(request.GET)
+    delimeter = dict_request.get('delimiter')[0] if dict_request.get('delimiter') else ','
+    expressions_list = dict_request['maths'][0].split(delimeter)
+    answers_list = list(map(eval_expr, expressions_list))
+
+    response = JsonResponse({x: y for x, y in list(zip(expressions_list, answers_list))})
+
+    return response
